936565b4bebff8ad4fa2c6b2194de7aec045498b
johnTjohansen <jtjohansen@ca.rr.com>
2017-03-23 01:49:34 -0700

    added previous search buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,13 @@
+  // declare global variables
+  var movieEntered = "";
+  var nameEntered = "";
+
+  var prevSearch = [];
+  var searchObj = {};
+  var searchText = "";
+
  // Initialize Firebase
-/*  var config = {
+  var config = {
     apiKey: "AIzaSyAO0b8UIE87PlrM4o1ouMnKLgdFCMaJgUg",
     authDomain: "team-mofo-project-1.firebaseapp.com",
     databaseURL: "https://team-mofo-project-1.firebaseio.com",
@@ -8,7 +16,7 @@
   };
   firebase.initializeApp(config);
 
-  var database = firebase.database();*/
+  var database = firebase.database();
 
   // When the submit button is pressed...
   $("#submit").on("click", function() {
@@ -20,43 +28,105 @@
     $("#directorNames").empty();
     $("#actorNames").empty();
 
-  	var movieEntered = $("#movieName").val().trim();
+  	movieEntered = $("#movieName").val().trim();
   	console.log(movieEntered)
 
+    buildNameButts();
+  });
+ 
+ // When a name button is pressed, search Youtube for related videos
+  $(document).on("click", ".nameButts", function() {
+    nameEntered = $(this).text();
+    console.log("name button clicked " + nameEntered);
+    updateFirebase();
+
+// youtube retrieval code goes here
+
+  });
+
+
+  // Firebase is always watching for changes to the data.
+  // When changes occurs it will print them to console and html
+  database.ref().on("value", function(snapshot) {
+    console.log(snapshot.val());
+    prevSearch = snapshot.val().sArray;
+    console.log(prevSearch);
+    $("#lastSearch").empty();
+    // build previous searches list
+    for (var i=0; i < prevSearch.length; i++) {
+      searchObj = prevSearch[i];
+      console.log(searchObj);
+      searchText = searchObj.sMovie + " - " + searchObj.sName;
+      $("#lastSearch").append("<button type='button' class='list-group-item prevButts'>" + searchText + "</button>");
+    };
+
+  });
+
+// Functions are here --------------------------------
+
+  function buildNameButts() {
     // construct our URL
     var queryURL = "http://www.omdbapi.com/?t=" + movieEntered + "&y=&plot=short&r=json";
-
     // Use $ajax to call OMDB API with movie title,
     // then take the response data and display directors and actors as buttons
     // for display in the name selection div.
-
     $.ajax({
        url: queryURL,
        method: "GET"
+
     }).done(function(response) {
+      if (response.Response != 'False') {
+        var responseData = JSON.stringify(response);
+        console.log(responseData);
+        var directors = response.Director.split(", ");
+        var actors = response.Actors.split(", ");
+        var posterURL = response.Poster;
+        console.log(directors);
+        console.log(actors);
+        console.log(posterURL);
 
-       var responseData = JSON.stringify(response);
-       console.log(responseData);
-       var directors = response.Director.split(", ");
-       var actors = response.Actors.split(", ");
-       console.log(directors);
-       console.log(actors);
+        for (var i=0; i < directors.length; i++) {
+          $("#directorNames").append("<button type='button' class='btn btn-default btn-lg btn-block nameButts'>" + directors[i] + "</button>");
+        };
 
-       for (var i=0; i < directors.length; i++) {
-       	 $("#directorNames").append("<button type='button' class='btn btn-default btn-lg btn-block'>" + directors[i] + "</button>");
+        for (var i=0; i < actors.length; i++) {
+          $("#actorNames").append("<button type='button' class='btn btn-default btn-lg btn-block nameButts'>" + actors[i] + "</button>");
+        };
 
-       };
+        $("#uTubes").empty();
+        $("#uTubes").append('<img src="' + posterURL + '" alt=poster>');
 
-       for (var i=0; i < actors.length; i++) {
-       	  $("#actorNames").append("<button type='button' class='btn btn-default btn-lg btn-block'>" + actors[i] + "</button>");
+        $("#movieName").val("");
+      } else {
+        $("#actorNames").append("<p>Search was not successful</p>");
+      }
+    })
+    .fail(function() {
+      console.log('error on afax call');
+    });  
+  };
 
-       $("#movieName").val() = "";
-       };
+
+  function updateFirebase() {
+    // create search key object to add to firebase
+    searchObj = {
+      sMovie: movieEntered,
+      sName: nameEntered
+    }; 
+    console.log(searchObj);
+
+    console.log("before " + prevSearch);
+    // add current search key to previous searches array.  remove last one
+    // if more than 5 objects
+    prevSearch.unshift(searchObj);
+    if (prevSearch.length > 5) {
+      prevSearch.pop();
+    };
+    console.log("after " + prevSearch);
+
+    // store modified array on firebase
+    database.ref().set({
+        sArray: prevSearch
     });
-// this is not working yet
-    $(document).ajaxError(function(){
-   	  console.log("ajax error"); 	
-      $("#actorNames").append("<p>Search was not successful</p>");
-    });
 
-  });
+  };
